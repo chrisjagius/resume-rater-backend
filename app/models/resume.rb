9b207ec56540c7e37e64caefe776433ce6df08ee
teasacura <teasacuraster@gmail.com>
2018-06-19 15:18:00 -0400

    actual update

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -4,7 +4,7 @@ class Resume < ApplicationRecord
 
   validates :title, presence: true
   validates :image_url, presence: true
-  validates :image_url, allow_blank: true, format: { with: %r{.(gif|jpg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
+  validates :image_url, allow_blank: true, format: { with: %r{.(gif|jpg|jpeg|png)\Z}i, message: 'must be a URL for GIF, JPG or PNG image.' }
   validates :industry, presence: true
   validates :user_id, presence: true
 end
