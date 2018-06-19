c8794afa15d5e26be60ed33122f5b29c3116bd0a
teasacura <teasacuraster@gmail.com>
2018-06-19 11:58:08 -0400

    adds validations to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :resume
   belongs_to :poster, class_name: "User"
+
+  validates :resume_id, presence: true
+  validates :poster_id, presence: true
+  validates :content, presence: true
+  validates :content, length: { maximum: 250 }
 end
